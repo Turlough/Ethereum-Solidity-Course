5f2f5cbac9f1c490afe51d323994c0a470f264f4
Turlough <turloughcowman@gmail.com>
2022-01-03 20:29:16 +0000

    Deploys first Inbox contract

---
+++ test/Inbox.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+//instantiate web3 on local ganache provider
+const web3 = new Web3(ganache.provider());
+//hook into the compiled Inbox code. Remember to compile first!
+const {interface, bytecode} = require('../compile');
+
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    //load the ganache test accounts
+    accounts = await web3.eth.getAccounts();
+    //use first account to deploy the contract on
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments:['Hi there!']})
+        .send({from: accounts[0], gas: '1000000'})
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
